f82ef3059b5449f7a60330ba12fb2369d1f8e578
Chung Loong Chan <hikoyanamikaze@gmail.com>
2021-12-07 23:52:20 +0800

    Added product page, add-to-cart function

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ config = {
   "apiKey": os.getenv("firebase_apiKey"), 
   "authDomain": os.getenv("authDomain"),
   "databaseURL": os.getenv("databaseURL"),
-  "storageBucket": os.getenv("storageBucket"),
-  "secretKey": os.getenv("secretKey")
+  "storageBucket": os.getenv("storageBucket")
 }
 
 client = typesense.Client({
@@ -30,6 +29,8 @@ client = typesense.Client({
     'connection_timeout_seconds': 2
 })
 
+<<<<<<< HEAD
+=======
 
 # Run this part during initial setup to create the typesense collection
 """
@@ -55,19 +56,60 @@ create_response = client.collections.create({
 })
 """
 
+>>>>>>> 7ee4e35c062cf3107de016750db60b1d564a4f26
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
-app.secret_key = config["secretKey"]
+app.secret_key = os.getenv("secretKey") or "supersecret123"
 
+def populate_typesense():
+    print("Populating collection...")
+    try:
+        products = db.child("products").get()
+        for p in products.each():          
+            data_typesense = {"id": p.val()['name'], "name": p.val()['name'], "price": p.val()['price'],
+                "sku": p.val()['sku'], "image": p.val()['image'], "created_at": p.val()['created_at']}
+            client.collections['products'].documents.create(data_typesense)
+            
+    except Exception as e:
+        print(e)
+                                                             
+# Run this part during initial setup to create the typesense collection
+def create_collection():
+    # Drop pre-existing collection if any
+    print("Creating collection..")
+    try:
+        client.collections['products'].delete()
+    except Exception as e:
+        print(e)
+
+    # Create a collection
+
+    create_response = client.collections.create({
+        "name": "products",
+        "fields": [
+            {"name": "id", "type": "string"},
+            {"name": "name", "type": "string"},
+            {"name": "price", "type": "float"},
+            {"name": "sku", "type": "string"},
+            {"name": "image", "type": "string"},
+            {"name": "created_at", "type": "float"}
+        ],
+        "default_sorting_field": "created_at"
+    })
+    
+    populate_typesense()
+    
+create_collection()
 # Retrive typesense collection
 client.collections['products'].retrieve()
+
 
 #Login
 @app.route("/")
 def login():
     if "email" in session:
-        return redirect(url_for('welcome'))
+        return redirect(url_for('products'))
     else:
         return render_template("login.html")
 
@@ -75,14 +117,6 @@ def login():
 @app.route("/signup")
 def signup():
     return render_template("signup.html")
-
-#Welcome page
-@app.route("/welcome")
-def welcome():
-    if "email" in session:
-        return render_template("welcome.html", email = session["email"])
-    else:
-        return render_template("login.html")
 
 #If someone clicks on login, they are redirected to /result
 @app.route("/result", methods = ["POST", "GET"])
@@ -100,7 +134,7 @@ def result():
             
         session['email'] = email
             
-        return redirect(url_for('welcome'))
+        return redirect(url_for('products'))
        
     else:
         return redirect(url_for('login'))
@@ -121,15 +155,15 @@ def register():
 
             session['email'] = email
             
-            #Go to welcome page
-            return redirect(url_for('welcome'))
+            #Go to main page
+            return redirect(url_for('products'))
         except:
             #If there is any error, redirect to register
             return redirect(url_for('register'))
 
     else:
         if 'email' in session:
-            return redirect(url_for('welcome'))
+            return redirect(url_for('products'))
         else:
             return redirect(url_for('register'))
 
@@ -138,14 +172,29 @@ def register():
 def logout():
    # remove the email from the session if it is there
    session.pop('email', None)
-   return redirect(url_for('welcome'))
+   return redirect(url_for('products'))
 
 ## Returns all products
-
 @app.route('/products', methods=['GET', 'POST'])
 def products():
     try:
         if request.method == 'GET':
+<<<<<<< HEAD
+            if 'email' in session:
+                try:
+                    products = db.child("products").get()
+                    output = []
+                    for p in products.each():
+                        output.append({"id": p.key(), "name": p.val()['name'], "price": p.val()['price'], "sku": p.val()[
+                                    'sku'], "image": p.val()['image'], "created_at": p.val()['created_at']})
+                    return render_template("welcome.html", email=session['email'], products=output)
+                except:
+                    return render_template("welcome.html", email=session['email'], products={"error": "No products found"})
+            else:  
+                return redirect(url_for('login'))
+                
+                
+=======
             try:
                 products = db.child("products").get()
                 output = []
@@ -155,15 +204,25 @@ def products():
                 return Response(json.dumps({"products": output}), status=200, mimetype='application/json')
             except:
                 return Response(json.dumps({"error": "No products found"}), status=400, mimetype='application/json')
+>>>>>>> 7ee4e35c062cf3107de016750db60b1d564a4f26
         else:
             ## Admin side function for us to populate the database with products
             if 'secretKey' in request.form:
-                if request.form['secretKey'] == os.getenv("secretKey"):
+                if request.form['secretKey'] == app.secret_key:
                     name = request.form['name']
                     price = float(request.form['price'])
                     sku = request.form['sku']
                     image = request.form['image']
                     created_at = time.time()
+<<<<<<< HEAD
+                    if image and sku and price and name and request.method == 'POST':
+                        try:
+                            data = {"name": name, "price": price,
+                                    "sku": sku, "image": image, "created_at": created_at}
+                            rec = db.child("products").push(data)
+                            data_typesense = {"id": rec['name'], "name": name, "price": price,
+                                            "sku": sku, "image": image, "created_at": created_at}
+=======
                     if created_at and image and sku and price and name and request.method == 'POST':
                         try:
                             data = {"name": name, "price": price,
@@ -171,6 +230,7 @@ def products():
                             rec = db.child("products").push(data) # push data to firebase realtime database
                             data_typesense = {"id": rec['name'], "name": name, "price": price,
                                             "sku": sku, "image": image, "created_at": created_at} #creates the same record in typesense collection, but with the unique timestamp key in firebase included
+>>>>>>> 7ee4e35c062cf3107de016750db60b1d564a4f26
                             client.collections['products'].documents.create(
                                 data_typesense)
                             return Response(json.dumps({"success": True}), status=200, mimetype='application/json')
@@ -235,5 +295,114 @@ def search(query):
     except Exception as e:
         return Response(json.dumps({"error": e}), status=400, mimetype='application/json')
    
+@app.route('/add', methods=['POST'])
+def add_to_cart():
+
+    _quantity = int(request.form['quantity'])
+    _code = request.form['sku']
+    
+    try:
+        products = client.collections['products'].documents.search({
+            'q': _code,
+            'query_by': 'sku',
+            'sort_by': 'created_at:desc'
+        })
+        try:
+  
+            for p in products['hits']:
+
+                itemArray = { _code : {'name' : p['document']['name'], 'sku' : _code, 'quantity' : _quantity, 'price' : p['document']['price'], 'image' :  p['document']['image'], 'total_price': _quantity * p['document']['price']}}
+         
+                all_total_price = 0
+                all_total_quantity = 0
+            
+                session.modified = True
+                if 'cart_item' in session:
+                    print(session['cart_item'])
+                    if _code in session['cart_item']:
+                        for key, value in session['cart_item'].items():
+                            if _code == key:
+                                old_quantity = session['cart_item'][key]['quantity']
+                                total_quantity = old_quantity + _quantity
+                                session['cart_item'][key]['quantity'] = total_quantity
+                                session['cart_item'][key]['total_price'] = total_quantity * p['document']['price']
+                    else:
+                        session['cart_item'] = array_merge(session['cart_item'], itemArray)
+
+                    for key, value in session['cart_item'].items():
+                        individual_quantity = int(session['cart_item'][key]['quantity'])
+                        individual_price = float(session['cart_item'][key]['total_price'])
+                        all_total_quantity = all_total_quantity + individual_quantity
+                        all_total_price = all_total_price + individual_price
+                
+                else:
+                    session['cart_item'] = itemArray
+                    all_total_quantity = all_total_quantity + _quantity
+                    all_total_price = all_total_price + _quantity * p['document']['price']
+                
+                session['all_total_quantity'] = all_total_quantity
+                session['all_total_price'] = all_total_price
+     
+            return redirect(url_for('products'))
+        except Exception as e:
+            print(e)
+            return redirect(url_for('products'))
+    except Exception as e:
+        print(e)
+        return redirect(url_for('products'))
+
+@app.route('/empty')
+def empty_cart():
+    try:
+        email = session['email']
+        session.clear()
+
+        session['email'] = email
+        return redirect(url_for('products'))
+    except Exception as e:
+        print(e)
+        
+@app.route('/delete/<string:code>')
+def delete_product(code):
+    try:
+        all_total_price = 0
+        all_total_quantity = 0
+        session.modified = True
+		
+        for item in session['cart_item'].items():
+            if item[0] == code:				
+                session['cart_item'].pop(item[0], None)
+                if 'cart_item' in session:
+                    for key, value in session['cart_item'].items():
+                        individual_quantity = int(session['cart_item'][key]['quantity'])
+                        individual_price = float(session['cart_item'][key]['total_price'])
+                        all_total_quantity = all_total_quantity + individual_quantity
+                        all_total_price = all_total_price + individual_price
+                break
+		
+        if all_total_quantity == 0:
+            email = session['email']
+            session.clear()
+
+            session['email'] = email
+        else:
+            session['all_total_quantity'] = all_total_quantity
+            session['all_total_price'] = all_total_price
+
+        return redirect(url_for('products'))
+    except Exception as e:
+        print(e)
+		
+def array_merge( first_array , second_array ):
+	if isinstance( first_array , list ) and isinstance( second_array , list ):
+		return first_array + second_array
+	elif isinstance( first_array , dict ) and isinstance( second_array , dict ):
+		return dict( list( first_array.items() ) + list( second_array.items() ) )
+	elif isinstance( first_array , set ) and isinstance( second_array , set ):
+		return first_array.union( second_array )
+	return False	
+
+
+       
 if __name__ == "__main__":
     app.run()
